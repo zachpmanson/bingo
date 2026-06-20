198e2521e439079c9f130d07db0fd70533c64862
Zach Manson <zachpmanson@gmail.com>
2026-06-20 15:47:30 +1000

    Initial nix

---
+++ flake.nix
@@ -0,0 +1,13 @@
+{
+  description = "bingo";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: {
+      packages.default = nixpkgs.legacyPackages.${system}.callPackage ./nix/package.nix {};
+    });
+}
---
+++ nix/package.nix
@@ -0,0 +1,35 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+let
+  pnpmDeps = pkgs.fetchPnpmDeps {
+    pname = "bingo";
+    version = "0.0.0";
+    src = ../.;
+    fetcherVersion = 3;
+    hash = pkgs.lib.fakeHash; # replace with the real hash from the first build error
+  };
+in
+
+pkgs.stdenv.mkDerivation {
+  pname = "bingo";
+  version = "0.0.0";
+  src = ../.;
+
+  nativeBuildInputs = [ pkgs.nodejs_24 pkgs.pnpm pkgs.pnpmConfigHook ];
+
+  inherit pnpmDeps;
+
+  buildPhase = ''
+    runHook preBuild
+    pnpm build
+    runHook postBuild
+  '';
+
+  # Nitro node-server output; run with: node $out/server/index.mjs
+  installPhase = ''
+    runHook preInstall
+    mkdir -p $out
+    cp -r .output/. $out/
+    runHook postInstall
+  '';
+}
