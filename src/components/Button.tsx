eddd31e7416915d8cd2c40dca64813e1515fc078
Zach Manson <zachpmanson@gmail.com>
2026-06-29 10:21:34 +1000

    Center button text

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -16,14 +16,14 @@ export default function Button(params: Params) {
     <button
       onClick={params.onClick}
       disabled={params.disabled}
-      className={`border-2 rounded bg-primary hover:bg-primary-hover active:bg-primary-active w-fit py-1 px-2 border-solid border-black disabled:opacity-50 disabled:cursor-not-allowed disabled:hover:bg-primary ${params.className ?? ''}`}
+      className={`text-center border-2 rounded bg-primary hover:bg-primary-hover active:bg-primary-active w-fit py-1 px-2 border-solid border-black disabled:opacity-50 disabled:cursor-not-allowed disabled:hover:bg-primary ${params.className ?? ''}`}
     >
       {params.children}
     </button>
   ) : (
     <Link
       {...params}
-      className={`border-2 rounded bg-blue-100 hover:bg-blue-200 active:bg-blue-300 w-fit py-1 px-2 border-solid border-black ${params.className}`}
+      className={`text-center border-2 rounded bg-blue-100 hover:bg-blue-200 active:bg-blue-300 w-fit py-1 px-2 border-solid border-black ${params.className}`}
     />
   );
 }
